2a969ace723c944968b1c3c7b056be97aa3b510c
Kiserexser <towartors@mail.ru>
2026-06-02 00:52:10 +0500

    Create FridonVisual.java

---
+++ src/main/java/fridon/visual/FridonVisual.java
@@ -0,0 +1,37 @@
+package fridon.visual;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class FridonVisual implements ClientModInitializer {
+
+    public static KeyBinding OPEN_GUI;
+
+    @Override
+    public void onInitializeClient() {
+
+        OPEN_GUI = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "Open Fridon Visual",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_RIGHT_SHIFT,
+                        "Fridon Visual"
+                )
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (OPEN_GUI.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.player.sendMessage(
+                            net.minecraft.text.Text.literal("Fridon Visual loaded!"),
+                            false
+                    );
+                }
+            }
+        });
+    }
+}
